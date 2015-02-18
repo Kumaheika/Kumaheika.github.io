f2af1051ba52fe81b43ceec8e4ff92dfd671dcd5
kuma <w87502047@gmail.com>
2015-02-18 13:21:24 +0800

    2015-02-18

--- jq/top.js
+++ jq/top.js
@@ -4,6 +4,14 @@ $(document).ready(function(){
 			scrollTop: 0
 		},400);
 	});
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 400 ){
+			$('.top').stop(true).fadeIn('fast');
+		} else {
+			$('.top').stop(true).fadeOut('fast');
+		}
+	});
+	//手機模式 收放Menu
 	$('.nav-icon').click(function(){
 		$('ul.navbar').toggleClass('nav-icon-show');
 	});
